3eddec62833bbc202608128eba3ab99fcda74337
Lucas A. Bonini <bonini.gfbpa@gmail.com>
2019-01-09 00:57:15 -0200

    more things added

--- Project.py
+++ Project.py
@@ -2,5 +2,13 @@
 import pandas as pd
 import numpy as np
 import tweepy
+import requests
+import json
 
 #%% 
+"""
+Download programmatically image_predictions.tsv
+https://d17h27t6h515a5.cloudfront.net/topher/2017/August/599fd2ad_image-predictions/image-predictions.tsv
+"""
+
+#%%
